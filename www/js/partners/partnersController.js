f084823a77bc612a75b63fae4db4b2fce0b1f845
Ryan <ryanmccombe@gmail.com>
2015-06-09 10:09:30 +0100

    Data Modelling

---
+++ www/js/partners/partnersController.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Ryan on 09/06/2015.
+ */
+
+angular.module('cultureTech')
+
+.controller('PartnersCtrl', ['$http', 'ionicMaterialInk', '$timeout', function($http, ionicMaterialInk, $timeout){
+        ionicMaterialInk.displayEffect();
+        var vm = this;
+        $http.get('js/2014.json').success(function(response){
+            vm.partners = response.partners;
+            $timeout(function() {
+                ionicMaterialInk.displayEffect();
+            }, 0);
+        })
+    }]);
