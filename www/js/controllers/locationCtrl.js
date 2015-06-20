a062ff1d273cf232fb2a5e6a5aef9137237c288d
Ryan <ryanmccombe@gmail.com>
2015-06-20 18:09:17 +0100

    Data connection handling

--- www/js/controllers/locationCtrl.js
+++ www/js/controllers/locationCtrl.js
@@ -3,14 +3,44 @@
  */
 
 angular.module('cultureTech')
-    .controller('LocationCtrl', ['locations', '$stateParams', '$timeout', 'ionicMaterialInk', 'uiGmapGoogleMapApi',
-        function (locations, $stateParams, $timeout, ionicMaterialInk, uiGmapGoogleMapApi) {
+    .controller('LocationCtrl', ['locations', '$stateParams', '$timeout', 'ionicMaterialInk', 'uiGmapGoogleMapApi', '$cordovaNetwork',
+        function (locations, $stateParams, $timeout, ionicMaterialInk, uiGmapGoogleMapApi, $cordovaNetwork) {
             $timeout(function () {
                 ionicMaterialInk.displayEffect();
             }, 0);
 
             var vm = this;
             vm.location = locations.data.locations[$stateParams.id];
+
+            // Network
+
+            vm.getNetwork = function(automatic){
+                vm.data = false;
+                vm.loading = false;
+
+                // $cordovaNetwork will be undefined in test environment
+                try {
+                    if ($cordovaNetwork.isOnline()) {
+                        vm.data = $cordovaNetwork.getNetwork();
+                    }
+                }
+                catch(err) {
+                    vm.data = true;
+                }
+
+                // Provide some feedback if user triggered it (fake loading screen for now)
+                if(!automatic){
+                    console.log('loading');
+                    vm.loading = true;
+                    $timeout(function () {
+                        console.log('done');
+                        vm.loading=false;
+                    }, 700);
+                }
+            };
+
+            vm.getNetwork(true);
+
 
             // Map UI
             vm.expandMap = false;
@@ -28,5 +58,5 @@ angular.module('cultureTech')
             });
 
             // TODO: Check data connection (cordova plugin)
-            vm.data = "3G";
+
         }]);
