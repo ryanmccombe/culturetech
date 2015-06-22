7a39b7b9d67458c7313eb7f3d5087d2cbeaad862
Ryan <ryanmccombe@gmail.com>
2015-06-22 14:52:31 +0100

    Info pages content

--- www/js/controllers/infoCtrl.js
+++ www/js/controllers/infoCtrl.js
@@ -3,29 +3,40 @@
  */
 
 angular.module('cultureTech')
-.controller('InfoCtrl', ['$ionicScrollDelegate', function($ionicScrollDelegate){
-        var vm = this;
-        vm.page = 0;
-        vm.menuExpanded = false;
-        vm.expandMenu = function () {
-            if (!vm.menuExpanded) {
-                vm.setMargin('0');
-                vm.menuExpanded = true;
-            } else {
-                vm.menuExpanded = false;
-            }
-        };
+    .controller('InfoCtrl', [
+        '$ionicScrollDelegate',
+        'ionicMaterialInk',
+        '$timeout',
+        function ($ionicScrollDelegate,
+                  ionicMaterialInk,
+                  $timeout) {
 
-        vm.setPage = function (page) {
-            if (vm.page != page) {
-                vm.page = page;
-                $ionicScrollDelegate.scrollTop(true);
-            }
-            vm.setMargin();
-        };
+            $timeout(function () {
+                ionicMaterialInk.displayEffect();
+            }, 0);
 
-        vm.setMargin = function (value) {
-            document.getElementById('innerDate').style.marginTop = value ? value : '-' + vm.page * 44 + 'px';
-        };
+            var vm = this;
+            vm.page = 0;
+            vm.menuExpanded = false;
+            vm.expandMenu = function () {
+                if (!vm.menuExpanded) {
+                    vm.setMargin('0');
+                    vm.menuExpanded = true;
+                } else {
+                    vm.menuExpanded = false;
+                }
+            };
 
-    }]);
+            vm.setPage = function (page) {
+                if (vm.page != page) {
+                    vm.page = page;
+                    $ionicScrollDelegate.scrollTop(true);
+                }
+                vm.setMargin();
+            };
+
+            vm.setMargin = function (value) {
+                document.getElementById('innerDate').style.marginTop = value ? value : '-' + vm.page * 44 + 'px';
+            };
+
+        }]);
