c6576ddd405a35e5e77d1e18e19b06399f54cf4a
Ryan <ryanmccombe@gmail.com>
2015-06-28 09:22:24 +0100

    Google map styling + map icon

--- www/js/controllers/locationCtrl.js
+++ www/js/controllers/locationCtrl.js
@@ -12,6 +12,81 @@ angular.module('cultureTech')
             var vm = this;
             vm.location = locations.data.locations[$stateParams.id];
 
+            var mapStyle = [
+                {
+                    "featureType": "landscape.natural",
+                    "stylers": [
+                        { "weight": 0.1 },
+                        { "color": "#ffffff" }
+                    ]
+                },{
+                    "featureType": "landscape.man_made",
+                    "stylers": [
+                        { "color": "#E1E6E8" }
+                    ]
+                },{
+                    "elementType": "labels.text.stroke",
+                    "stylers": [
+                        { "visibility": "on" },
+                        { "color": "#ffffff" }
+                    ]
+                },{
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        { "color": "#121A21" }
+                    ]
+                },{
+                    "featureType": "water",
+                    "stylers": [
+                        { "visibility": "on" },
+                        { "color": "#f4f4f4" }
+                    ]
+                },{
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        { "visibility": "off" }
+                    ]
+                },{
+                    "elementType": "labels.text.stroke",
+                    "stylers": [
+                        { "weight": 8 }
+                    ]
+                },{
+                    "featureType": "road.highway",
+                    "elementType": "geometry",
+                    "stylers": [
+                        { "visibility": "simplified" },
+                        { "color": "#363F45" }
+                    ]
+                },{
+                    "featureType": "road.arterial",
+                    "elementType": "geometry",
+                    "stylers": [
+                        { "color": "#94A0A8" }
+                    ]
+                },{
+                    "featureType": "road.local",
+                    "elementType": "labels.text.stroke",
+                    "stylers": [
+                        { "color": "#ffffff" },
+                        { "weight": 4.3 }
+                    ]
+                },{
+                    "featureType": "poi",
+                    "stylers": [
+                        { "visibility": "off" }
+                    ]
+                },{
+                }
+            ];
+
+            vm.mapOptions = {
+                styles: mapStyle,
+                disableDefaultUI: true
+            };
+
+            vm.markerIcon= { url:"../img/mapmarker.png" };
+
 
             // Network
             vm.getNetwork = function(automatic){
